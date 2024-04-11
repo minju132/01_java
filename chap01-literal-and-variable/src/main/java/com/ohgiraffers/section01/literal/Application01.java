28eb3eaf94e3ec6718ef59936edb5beb7eddf5fb
zoo <minju132123@gmail.com>
2024-04-11 18:01:30 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+        // 리터럴이란 값 자체를 의미, 문자 그대로의
+
+        // 정수형 리터럴
+        System.out.println(132);
+        // 실수형 리터럴
+        System.out.println(1.5);
+        // 문자형 리터럴
+        System.out.println('A');
+        // 문자열형 리터럴
+        System.out.println("안녕하세요");
+
+        System.out.println("hello world");
+
+        System.out.println(true);
+        System.out.println(false);
+        //did you see me???
+        System.out.println("I see you");
+    }
+}
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application02 {
+    public static void main(String[] args){
+
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        System.out.println(123%10);
+        System.out.println(2);
+        System.out.println(2 / 0);
+
+        System.out.println(0/2);
+        System.out.println(123/10);
+        System.out.println(1.23+1.23);
+        System.out.println(1.23-1.23);
+        System.out.println(1.23*1.23);
+        System.out.println(1.23/1.23);
+        System.out.println(1.23%1.0);
+        System.out.println("123"+"12");
+    }
+}
